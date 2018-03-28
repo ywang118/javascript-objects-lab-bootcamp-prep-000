8ef41880c0f9add1227b7408b5d631a46be69930
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-28 21:23:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(object,key, value) {
   return object;
 }
 
-function 
+function  updateObjectWithKeyAndValue(object,key, value) {
+  return Object.assign({},object, {[key]: value})
+}
