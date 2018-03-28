f8a04c2c6e636774151404e9e16ccda6ae00f6fd
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-28 21:25:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function  updateObjectWithKeyAndValue(object,key, value) {
   return Object.assign({},object, {[key]: value})
 }
 
-funtion 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+}
