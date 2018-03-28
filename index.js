d76fdee0c7f2cac280aad46b622c6327d20f4ee5
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-03-28 21:28:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-function 
+function  deleteFromObjectByKey(object, key) {
+  var object_new = Object.assign({},object)
+  delete object_new[key]
+  return object_new;
+}
